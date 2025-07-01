a5142b7ace9c224648468004a005652ceb92def7
Luizsaw <luizsaw@gmail.com>
2025-06-30 21:39:14 -0300

    Adiciona um arquivo .gitignore

--- obj/Debug/net8.0/SistemaHospedagemHotel.AssemblyInfo.cs
+++ obj/Debug/net8.0/SistemaHospedagemHotel.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaHospedagemHotel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a8dc20da683d5af6e98e1fda74c9065894c1cba")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaHospedagemHotel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaHospedagemHotel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
